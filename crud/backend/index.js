2ae7af764ad459593d601e9b6fde7928ea7f6c06
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2023-09-14 20:33:32 +0200

    login, register

--- crud/backend/index.js
+++ crud/backend/index.js
@@ -1,17 +1,31 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { db } from "./connection.js";
 
 // Import routes
 import booksRoute from "./routes/books.js";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
+
+// Allow access to client side
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 // JSON middleware
 app.use(express.json());
 
 //CORS middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 // Connect with DB
 //const db = connection.connect();
@@ -25,6 +39,7 @@ app.get("/", (req, res) => {
 
 // Books route
 app.use("/api/books", booksRoute);
+app.use("/api/auth", authRoutes);
 
 app.listen(8800, () => {
   console.log("Connected to backend.");
